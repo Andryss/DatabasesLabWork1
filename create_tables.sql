cd89a49eb48015f41570be597ea87a8ed5ee3ec0
Andryss <andr.krivosheev@gmail.com>
2023-09-09 23:06:09 +0300

    Add create_tables.sql

---
+++ create_tables.sql
@@ -0,0 +1,26 @@
+create table if not exists humans (
+    id bigint primary key,
+    name varchar(20) unique not null
+);
+
+create table if not exists painters (
+    id bigint primary key,
+    human_id bigint references humans(id),
+    assignment_date date not null,
+    expiration_date date not null
+);
+
+create table if not exists houses (
+    id bigint primary key,
+    address varchar(256) unique not null,
+    description varchar(512)
+);
+
+create table if not exists pictures (
+    id bigint primary key,
+    house_id bigint references houses(id),
+    painter_id bigint references painters(id) not null,
+    created_date date not null,
+    description varchar(512) not null,
+    place varchar(512)
+);
